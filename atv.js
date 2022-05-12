9b910152d8a78464118559649ad1aa7ac981fbc9
Brunoomiguell <103513090+Brunoomiguell@users.noreply.github.com>
2022-05-12 11:13:49 -0300

    Add files via upload

---
+++ atv.js
@@ -0,0 +1,22 @@
+  //atv01
+
+//let idade  = prompt('Digite sua Idade')
+//if(idade > 16){
+  //  alert('voce ainda nao pode votar')
+//}
+ 
+//atv02
+
+//let senha = prompt('Digite sua senha')
+//if(senha == 1234){
+ //   alert('ACESSO PERMITIDO')
+//}else{
+  //alert('ACESSO NEGADO')
+//}
+ 
+//atv03
+let periodo = prompt('Digite seu periodo')
+if (periodo == 'M')alert('Bom dia !');
+else if (periodo == 'V')alert ('Boa tarde !');
+else if (periodo == 'N')alert('Boa noite !');
+else final = alert('indefinido');
